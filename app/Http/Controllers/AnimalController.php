8c5074dca25e4bc8394c330722dfd5fdee2f9906
Elsa <sasa.milhet@gmail.com>
2024-03-12 17:19:52 +0100

    animaux

---
+++ app/Http/Controllers/AnimalController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Animal;
+
+class AnimalController extends Controller
+{
+    // Liste tous les animaux
+    public function list(Request $request)
+    {
+        $animaux = Animal::get();
+        return response()->json($animaux);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Animal;
+use App\Http\Controllers\AnimalController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Liste tous les animaux
+Route::get('/animaux', [AnimalController::class, 'list']);
